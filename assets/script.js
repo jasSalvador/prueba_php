fb57d92cb02fa8ee0fbb01b954ab0ff3bcc35de8
Jasmin Salvador <sjasmin.salvador@gmail.com>
2023-12-02 22:04:44 -0400

    actualizando

--- assets/script.js
+++ assets/script.js
@@ -48,192 +48,213 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 //ABRIR MODAL FORM
-const btnCotizar = document.querySelector('#btnCotizar');
+/* const btnCotizar = document.querySelector('#btnCotizar');
 const modalForm = document.querySelector('#modalForm');
-//const btnEnviar = document.querySelector('#btnEnviar');
 
 btnCotizar.addEventListener('click', (e) => {
     e.preventDefault();
-    //console.log("boton clickeado");
+    console.log("boton clickeado");
     modalForm.classList.add("active");
 });
 
-//FORMULARIO
-async function handleSubmit(event) {
-    event.preventDefault();
 
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const comuna = document.getElementById('comuna').value;
-    const fecha = document.getElementById('fecha').value;
-    const asistentes = document.getElementById('asistentes').value;
-    const inicio = document.getElementById('inicio').value;
-    const termino = document.getElementById('termino').value;
-    const pack = document.getElementById('pack').value;
-    const mensaje = document.getElementById('mensaje').value;
-
-    const formData = {
-        nombre,
-        apellido,
-        email,
-        comuna,
-        fecha,
-        asistentes,
-        inicio,
-        termino,
-        pack,
-        mensaje
-    };
-
-    try {
-        const response = await fetch('https://www.rqsproducciones.cl/send-email.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-
-        const result = await response.json();
-        console.log(result);
-
-        if (result.status === 'success') {
-            alert('Mensaje enviado con éxito.');
-        } else {
-            alert('Hubo un error al enviar el mensaje.');
-        }
-    } catch (error) {
-        console.error('Error general:', error);
-        alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
-    }
-}
+const form = document.getElementById('formCotizacion');
+console.log('Formulario seleccionado:', form);
+form.addEventListener('submit', handleSubmit); */
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const btnCotizar = document.querySelector('#btnCotizar');
+    const modalForm = document.querySelector('#modalForm');
+    const form = document.getElementById('formCotizacion');
 
+    btnCotizar.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log("Botón clickeado");
+        modalForm.classList.add("active");
+    });
 
+    form.addEventListener('submit', handleSubmit);
 
+    //FORMULARIO
+    async function handleSubmit(event) {
+        console.log('Evento de envío del formulario capturado');
+        event.preventDefault();
+        console.log('Formulario enviado');
 
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const comuna = document.getElementById('comuna').value;
+        const fecha = document.getElementById('fecha').value;
+        const asistentes = document.getElementById('asistentes').value;
+        const inicio = document.getElementById('inicio').value;
+        const termino = document.getElementById('termino').value;
+        const pack = document.getElementById('pack').value;
+        const mensaje = document.getElementById('mensaje').value;
 
+        const formData = {
+            nombre,
+            apellido,
+            email,
+            comuna,
+            fecha,
+            asistentes,
+            inicio,
+            termino,
+            pack,
+            mensaje
+        };
 
-
-
-
-
-
-/* function handleSubmit(event) {
-    event.preventDefault();
-
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const comuna = document.getElementById('comuna').value;
-    const fecha = document.getElementById('fecha').value;
-    const asistentes = document.getElementById('asistetes').value;
-    const inicio = document.getElementById('inicio').value;
-    const termino = document.getElementById('termino').value;
-    const pack = document.getElementById('pack').value;
-    const mensaje = document.getElementById('mensaje').value;
-
-    const formData = {
-        nombre,
-        apellido,
-        email,
-        comuna,
-        fecha,
-        asistentes,
-        inicio,
-        termino,
-        pack,
-        mensaje
-    };
-
-    try {
-        fetch('https://www.rqsproducciones.cl/send-email.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
-            .then(response => response.json())
-            .then(result => {
-                if (result.status === 'success') {
-                    alert('Mensaje enviado con éxito.');
-                    document.getElementById('nombre').value = '';
-                    document.getElementById('apellido').value = '';
-                    document.getElementById('email').value = '';
-                    document.getElementById('comuna').value = '';
-                    document.getElementById('fecha').value = '';
-                    document.getElementById('asistentes').value = '';
-                    document.getElementById('inicio').value = '';
-                    document.getElementById('termino').value = '';
-                    document.getElementById('pack').value = '';
-                    document.getElementById('mensaje').value = '';
-                } else {
-                    alert('Hubo un error al enviar el mensaje.');
-                }
+        try {
+            const response = await fetch('https://www.rqsproducciones.cl/send-email.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
             });
-    } catch (error) {
-        console.error('Error general:', error);
-        alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
-    }
-}
- */
 
+            const result = await response.json();
+            console.log(result);
 
-
-
-
-
-//carrusel
-const carrusel = document.querySelector(".carrusel-imagenes");
-let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
-let intervalo = null;
-let step = 1.5;
-
-const start = () => {
-    intervalo = setInterval(function () {
-        carrusel.scrollLeft += step;
-
-        if (carrusel.scrollLeft >= maxScrollLeft) {
-            carrusel.scrollLeft = 0;
+            if (result.status === 'success') {
+                alert('Mensaje enviado con éxito.');
+            } else {
+                alert('Hubo un error al enviar el mensaje.');
+            }
+        } catch (error) {
+            console.error('Error general:', error);
+            alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
         }
-    }, 10);
-};
-
-const stop = () => {
-    clearInterval(intervalo);
-};
-
-const prevButton = document.querySelector(".carrusel-prev");
-const nextButton = document.querySelector(".carrusel-next");
-
-prevButton.addEventListener("click", () => {
-    stop();
-    carrusel.scrollLeft -= 400;
+    }
 });
 
-nextButton.addEventListener("click", () => {
-    stop();
-    carrusel.scrollLeft += 400;
-});
 
-carrusel.addEventListener("mouseover", () => {
-    stop();
-});
 
-carrusel.addEventListener("mouseout", () => {
-    start();
-});
 
-document.querySelectorAll("a").forEach((enlace) => {
-    enlace.addEventListener("click", () => {
+
+
+
+
+
+
+
+
+
+    /* function handleSubmit(event) {
+        event.preventDefault();
+    
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const comuna = document.getElementById('comuna').value;
+        const fecha = document.getElementById('fecha').value;
+        const asistentes = document.getElementById('asistetes').value;
+        const inicio = document.getElementById('inicio').value;
+        const termino = document.getElementById('termino').value;
+        const pack = document.getElementById('pack').value;
+        const mensaje = document.getElementById('mensaje').value;
+    
+        const formData = {
+            nombre,
+            apellido,
+            email,
+            comuna,
+            fecha,
+            asistentes,
+            inicio,
+            termino,
+            pack,
+            mensaje
+        };
+    
+        try {
+            fetch('https://www.rqsproducciones.cl/send-email.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+                .then(response => response.json())
+                .then(result => {
+                    if (result.status === 'success') {
+                        alert('Mensaje enviado con éxito.');
+                        document.getElementById('nombre').value = '';
+                        document.getElementById('apellido').value = '';
+                        document.getElementById('email').value = '';
+                        document.getElementById('comuna').value = '';
+                        document.getElementById('fecha').value = '';
+                        document.getElementById('asistentes').value = '';
+                        document.getElementById('inicio').value = '';
+                        document.getElementById('termino').value = '';
+                        document.getElementById('pack').value = '';
+                        document.getElementById('mensaje').value = '';
+                    } else {
+                        alert('Hubo un error al enviar el mensaje.');
+                    }
+                });
+        } catch (error) {
+            console.error('Error general:', error);
+            alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+        }
+    }
+     */
+
+
+
+
+
+
+    //carrusel
+    const carrusel = document.querySelector(".carrusel-imagenes");
+    let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+    let intervalo = null;
+    let step = 1.5;
+
+    const start = () => {
+        intervalo = setInterval(function () {
+            carrusel.scrollLeft += step;
+
+            if (carrusel.scrollLeft >= maxScrollLeft) {
+                carrusel.scrollLeft = 0;
+            }
+        }, 10);
+    };
+
+    const stop = () => {
+        clearInterval(intervalo);
+    };
+
+    const prevButton = document.querySelector(".carrusel-prev");
+    const nextButton = document.querySelector(".carrusel-next");
+
+    prevButton.addEventListener("click", () => {
+        stop();
+        carrusel.scrollLeft -= 400;
+    });
+
+    nextButton.addEventListener("click", () => {
+        stop();
+        carrusel.scrollLeft += 400;
+    });
+
+    carrusel.addEventListener("mouseover", () => {
         stop();
     });
-});
 
-start();
+    carrusel.addEventListener("mouseout", () => {
+        start();
+    });
+
+    document.querySelectorAll("a").forEach((enlace) => {
+        enlace.addEventListener("click", () => {
+            stop();
+        });
+    });
+
+    start();
 
 
 
